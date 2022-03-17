14bddee65df3df9af40d9a60688f9ff4a60c2d73
Emma Pagnon <emma.pagnon@ceff.ch>
2022-03-17 13:10:58 +0100

    1.1.13

--- droits/json/add_autorisation.json.php
+++ droits/json/add_autorisation.json.php
@@ -1,3 +1,13 @@
 <?php
 session_start();
 print_r($_POST);
+
+
+
+$tab = array();
+$tab['nom_auto'] = "";
+$tab['code_auto'] = "";
+$tab['description_admin_auto'] = "";
+$tab['description_user_auto'] = "";
+
+
